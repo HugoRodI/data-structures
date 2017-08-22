eb04e9d05e41eee2a36c46cac4a76851defd996f
HugoRod <hugo.rodriguezignacio@gmail.com>
2017-08-22 22:54:01 +0200

    Add list reverse using stacks.
    Add list-reverse.c, a list reverse function with stacks using arrays.
    
    Changes to be committed:
        new file:   infix-to-postfix.c
        new file:   infix-to-prefix.c
        new file:   list-reverse.c
        new file:   parenthesis-checker.c
        new file:   postfix-eval.c
        new file:   prefix-eval.c
        new file:   recursion.c
        new file:   tower-of-hanoi.c

---
+++ stacks/list-reverse.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+#define SIZE 1
+
+int st[SIZE];
+int top = -1;
+void push(int);
+int pop();
+
+int main()
+{
+    int inversed[SIZE], i;
+
+    for (i = 0; i < SIZE; ++i)
+    {
+        printf("\n Enter the element %d of the array: ", i + 1);
+        scanf("%d", &st[i]);
+        push(st[i]);
+    }
+
+    printf("\n The input array is: ");
+
+    for (i = 0; i < SIZE; ++i)
+        printf("\n %d", st[i]);
+
+    for (i = 0; i < SIZE; ++i)
+        inversed[i] = pop();
+
+    printf("\n The inversed array is: ");
+
+    for (i = 0; i < SIZE; ++i)
+        printf("\n %d", inversed[i]);
+
+
+    return 0;
+}
+
+void push(int val)
+{
+    st[++top] = val;
+}
+
+int pop()
+{
+    return st[top--];
+}
